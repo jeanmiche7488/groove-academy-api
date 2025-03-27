8d612304a2680dfe47ca51239edcbcd2f32c869f
Pierre Servant <pierre.servant5@gmail.com>
2025-03-27 17:03:51 +0100

    chore: Suppression des fichiers inutilisés

--- src/routes/stats.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { statsController } from '../controllers/stats.controller';
-
-const router = express.Router();
-
-router.get('/dashboard', statsController.getDashboardStats);
-
-export default router; 
